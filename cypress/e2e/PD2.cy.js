f0556f83e7e4bbb4fd2b7501d7ce6141c7aa5896
xDagomex <adaskubiak@wp.pl>
2024-10-28 04:00:20 +0100

    4

---
+++ cypress/e2e/PD2.cy.js
@@ -0,0 +1,20 @@
+describe("User can visit GoIT page", () => {
+    beforeEach("Go to page", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+    })
+    it("First log in", () => {
+        cy.get('#user_email').type('user888@gmail.com')
+        cy.get('input[type=password').type('1234567890')
+        cy.get('.eckniwg2').click()
+        cy.get('#open-navigation-menu-mobile').click()
+        cy.get(':nth-child(12) > .next-bve2vl').click()
+    })
+
+    it("Second log in", () => {
+        cy.get('#user_email').type('testowyqa@qa.team')
+        cy.get('input[type=password').type('QA!automation-1')
+        cy.get('.eckniwg2').click()
+        cy.get('#open-navigation-menu-mobile').click()
+        cy.get(':nth-child(9) > .next-bve2vl').click()
+    })
+})
