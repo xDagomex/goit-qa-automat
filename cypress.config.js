dca4890f67e640946fe03f29b5dff8b414ec4010
xDagomex <adaskubiak@wp.pl>
2024-10-30 02:55:54 +0100

    31

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/e2e/**/*.cy.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
